59e1d5d207192bcb528ce3d6a2f67b740f355a4c
Ratna Dewanti Astutingtias <ratnadewanti962@gmail.com>
2025-02-03 11:06:19 +0700

    Add files via upload

---
+++ portofolio-ratna dewanti/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".typing-text", {
+    strings: ["Student at Polythecnic LP3I Jakarta", "into Web Development"],
+    loop: true,
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 500,
+  });
